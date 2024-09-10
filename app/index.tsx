a01ebc579e7ac984ce4b8777150e8316cb088dbc
Eirik <eirik.imrik@gmail.com>
2024-09-10 20:42:23 +0200

    added linear background

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet, Dimensions } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NativeWindStyleSheet } from "nativewind";
+import { LinearGradient } from "expo-linear-gradient";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -11,6 +12,14 @@ export default function Index() {
     fullScreen: {
       width: Dimensions.get("window").width,
       height: Dimensions.get("window").height,
+    },
+  });
+  const gradientStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      justifyContent: "center",
+      alignItems: "center",
     },
   });
 
@@ -26,7 +35,14 @@ export default function Index() {
   return (
     <SafeAreaProvider>
       <View style={viewStyles.container}>
+      <LinearGradient
+          colors={["#2193b0", "#6dd5ed",]}
+          start={[0, 0]}
+          end={[1, 1]}
+          style={gradientStyles.container}
+        >
         <Text>Edit app/index.tsx to edit this screen.</Text>
+        </LinearGradient>
       </View>
     </SafeAreaProvider>
   );
