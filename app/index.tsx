a8b67d6a672580692290bb1608e26dcf8d31b67f
Eirik <eirik.imrik@gmail.com>
2024-09-11 19:04:31 +0200

    added support for navigation and added first page

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { Text, View, StyleSheet, Dimensions, Button } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NativeWindStyleSheet } from "nativewind";
 import { LinearGradient } from "expo-linear-gradient";
+import { Link } from "expo-router";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
 });
 
 export default function Index() {
-  
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -18,26 +18,26 @@ export default function Index() {
     textStyles: {
       fontSize: 14,
       color: "white",
-
     },
     gradient: {
       flex: 1,
       width: "100%",
       justifyContent: "center",
       alignItems: "center",
-    }
-  })
+    },
+  });
 
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
-      <LinearGradient
-          colors={["#2193b0", "#6dd5ed",]}
+        <LinearGradient
+          colors={["#2193b0", "#6dd5ed"]}
           start={[0, 0]}
           end={[1, 1]}
           style={styles.gradient}
         >
-        <Text style={styles.textStyles}>Edit app/index.tsx to edit this screen.</Text>
+          <Text style={styles.textStyles}>This is the quiz app.</Text>
+          <Link href="/nextpage">Start Quiz</Link>
         </LinearGradient>
       </View>
     </SafeAreaProvider>
---
+++ app/nextpage.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const nextpage = () => {
+  return (
+    <View>
+        <Text>First page</Text>
+    </View>
+  )
+}
+
+export default nextpage
