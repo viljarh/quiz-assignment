17136123d572a48e4869cb349157378e6d359c8b
Eirik <eirik.imrik@gmail.com>
2024-09-13 14:42:51 +0200

    added seperate data file to store the questions

---
+++ app/data/QuizData.ts
@@ -0,0 +1,36 @@
+
+const quizData = [
+  {
+    question: "What does React Native use instead of css?",
+    options: ["Javascript", "HTML", "Figma", "XML"],
+    correct: "Javascript",
+  },
+  {
+    question: "Which company developed React Native?",
+    options: ["Google", "Apple", "Microsoft", "Facebook"],
+    correct: "Facebook",
+  },
+  {
+    question: "What component is used to display text in React Native?",
+    options: ["View", "Image", "TextInput", "Text"],
+    correct: "Text",
+  },
+  {
+    question: "What layout system does react native used by default?",
+    options: ["Grid", "Bootstrap", "Material UI", "Flexbox"],
+    correct: "Flexbox",
+  },
+  {
+    question:
+      "What hook is used to manage state in a functional component in React Native?",
+    options: ["useState", "useEffect", "useContext", "useReducer"],
+    correct: "useState",
+  },
+  {
+    question: "What is the styling library used in React Native?",
+    options: ["CSS", "SASS", "Styled Components", "StyleSheet"],
+    correct: "StyleSheet",
+  },
+];
+
+export default quizData;
