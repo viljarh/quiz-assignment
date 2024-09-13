005f2a8e746d883536300ff3483520cf5b587cce
Eirik <eirik.imrik@gmail.com>
2024-09-13 17:05:18 +0200

    samll fix

--- app/components/QuizComponent.tsx
+++ app/components/QuizComponent.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, Pressable } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useState } from "react";
 import quizData from "../data/QuizData";
 import styles from "@/constants/Styles";
